8400834f92e01b3b5e2e1b5edc8c64e6172788bc
Ha Nul <sky128kbp@gmail.com>
2024-03-08 16:25:05 +0900

    add const.h

--- C++/2024_03_06_coupfunc.cpp
+++ C++/2024_03_06_coupfunc.cpp
@@ -30,35 +30,35 @@ vector<double> linspace(const double& min, const double& max, int n)
         return result;
     }
 
-vector<double> mode_arr = linspace(0,10,10);
+vector<double> omega_arr = linspace(1,10,10);
+vector<double> tilde_g_arr = linspace(1,10,10);
 vector<double> tau_arr = linspace(0,1,100);
-double omega = 1;
 
-void g_k_calculation_function(double alpha, double k_cutoff, double mode)
+void tilde_g_calculation_function(double alpha, double k_cutoff)
 {
     double nu = pi * k_cutoff / alpha;
 
-    for (int i=0; i<mode_arr.size(); i++)
+    for (int i=0; i<omega_arr.size(); i++)
     {
-        mode_arr[i] = (1/planck_cst) * sqrt((2 * planck_cst * k_cutoff / alpha * mode) * (planck_cst * mode_arr[i] / 1 + pow(nu * planck_cst * mode_arr[i] / k_cutoff,2)));
+        omega_arr[i] = k_cutoff * (omega_arr[i]/omega_arr[omega_arr.size()-1]);
+        //tilde_g_arr[i] = sqrt( (omega_arr[i] / (1 + pow(nu * omega_arr[i] / k_cutoff,2))));
+        tilde_g_arr[i] = sqrt((2 * k_cutoff / (alpha * omega_arr.size())) * (planck_cst * omega_arr[i] / (1 + pow(nu * planck_cst * omega_arr[i] / k_cutoff,2))));
     }
 }
 
-vector<double> V_function(vector<double>coupling, vector<double> tau, double omega)
+vector<double> Interact_V(vector<double> tau)
 {
-    vector<double> hpcos(tau.size(), 0);
-    vector<double> hpsin(tau.size(), 0);
     vector<double> V_arr(tau.size(), 0);
 
     for (int i = 0; i < tau.size(); i++)
     {
-        hpcos[i] = cosh((tau[i] - tau[tau.size() - 1] / 2) * omega);
-        hpsin[i] = sinh(tau[tau.size() - 1] * omega / 2);
-        for (int j = 0; j < coupling.size();j++)
+        for (int j = 0; j < tilde_g_arr.size();j++)
         {
-            V_arr[i] += (pow(coupling[j],2) * hpcos[i] / hpsin[i]);
+            V_arr[i] += pow(tilde_g_arr[j],2) * cosh((tau[i] - tau[tau.size() - 1] / 2) * omega_arr[j])/sinh(tau[tau.size() - 1] * omega_arr[j] / 2);
+            //cout << "\t" << j <<" V_arr : " << V_arr[i] << " with tau-beta/2 : " << tau[i] - tau[tau.size()-1]/2 << endl;
         }
         cout << "this is V_arr " << V_arr[i] << endl;
+        cout << setprecision(16);
     }
 
     return V_arr;
@@ -66,8 +66,34 @@ vector<double> V_function(vector<double>coupling, vector<double> tau, double ome
 
 int main()
 {
-    g_calculation_function(1,10,10);
-    Interact_V(mode_arr,tau_arr,omega);
+    double k_cutoff = planck_cst * 10e20;
+    for (int i=0; i<omega_arr.size(); i++)
+    {
+        cout << omega_arr[i] << endl;
+    }
+
+    tilde_g_calculation_function(0.5,k_cutoff);
+    vector<double> a = Interact_V(tau_arr);
+
+    std::ofstream outputFile;
+    string name = "coupTEST";
+    name += ".txt";
+
+    outputFile.open(name);
+
+    for (int j = 0; j < tilde_g_arr.size(); j++)
+    {
+        cout << a[j] << endl;
+        outputFile << tau_arr[j] << "\t" << a[j] << endl;
+    }
+    
+
+    outputFile.close();
+/*    for (int i=0; i<omega_arr.size(); i++)
+    {
+        cout << g_arr[i] << endl;
+    }
+*/
 
     return 0;
 }
